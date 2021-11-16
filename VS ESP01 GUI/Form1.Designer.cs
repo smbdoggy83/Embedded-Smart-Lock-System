6ab552440edbe94cd2ffe64bc3eb6f276e9eb37f
Seth Brodzik <sethmb83@gmail.com>
2021-11-16 09:07:16 -0500

    VS Code given by "Tutorial.docx" implemented

--- VS ESP01 GUI/Form1.Designer.cs
+++ VS ESP01 GUI/Form1.Designer.cs
@@ -29,12 +29,112 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(59, 91);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 71);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Serial Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(376, 91);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(175, 71);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Serial Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(59, 197);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(180, 72);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Read in TS Channel";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(436, 234);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(129, 341);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Current Data";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(407, 341);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.SerialPort1_DataReceived);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.IO.Ports.SerialPort serialPort1;
     }
 }
 
--- VS ESP01 GUI/Form1.cs
+++ VS ESP01 GUI/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,135 @@ namespace VS_ESP01_GUI
 {
     public partial class Form1 : Form
     {
+        string portName = "COM4"; // ENTER PORT HERE EVERY TIME YOU RE-PLUG IN THE MSP430
+
+        int startflag = 0;
+        int flag_sensor;
+        string RxString;
+        string temp = "30";
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+                serialPort1.Close();
+            serialPort1.PortName = portName;
+            serialPort1.BaudRate = 9600;
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SerialStart_Click(object sender, EventArgs e)
+        {
+            serialPort1.PortName = "COM6";
+            serialPort1.BaudRate = 9600;
+            serialPort1.Open();
+            if (serialPort1.IsOpen)
+            {
+                // startSerial.Enabled = false;
+                // serialStop.Enabled = true;
+                textBox1.ReadOnly = false;
+            }
+        }
+
+        private void SerialStop_Click(object sender, EventArgs e)
+        {
+            serialPort1.Close();
+            //  startSerial.Enabled = true;
+            //  serialStop.Enabled = false;
+            textBox1.ReadOnly = true;
+        }
+
+        private void Read_in_channel_Click(object sender, EventArgs e)
+        {
+            WebClient client = new WebClient();
+
+            label1.Text = client.DownloadString("http://api.thingspeak.com/channels/1503667/field/field1/last.text");
+        }
+
+        private void Current_data_Click(object sender, EventArgs e)
+        {
+            textBox1.AppendText(RxString);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (!string.Equals(textBox1.Text, ""))
+            {
+                if (serialPort1.IsOpen) serialPort1.Close();
+                try
+                {
+                    /*  if (RxString[0] == 'B')
+                      {
+                          flag_sensor = 10;
+                      }*/
+                    const string WRITEKEY = "3RL1K710Z99NAPPH";
+                    string strUpdateBase = "http://api.thingspeak.com/update";
+                    string strUpdateURI = strUpdateBase + "?api_key=" + WRITEKEY;
+                    string strField1 = textBox1.Text;
+                    HttpWebRequest ThingsSpeakReq;
+                    HttpWebResponse ThingsSpeakResp;
+                    strUpdateURI += "&field1=" + strField1;
+
+                    flag_sensor++;
+                    ThingsSpeakReq = (HttpWebRequest)WebRequest.Create(strUpdateURI);
+                    ThingsSpeakResp = (HttpWebResponse)ThingsSpeakReq.GetResponse();
+                    ThingsSpeakResp.Close();
+                    if (!(string.Equals(ThingsSpeakResp.StatusDescription, "OK")))
+                    {
+                        Exception exData = new Exception(ThingsSpeakResp.StatusDescription);
+                        throw exData;
+                    }
+                }
+                catch (Exception ex)
+                {
+                }
+                textBox1.Text = "";
+                serialPort1.Open();
+            }
+        }
+
+        private void SerialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            RxString = serialPort1.ReadExisting();
+            this.Invoke(new EventHandler(Current_data_Click));
+        }
+
     }
+
 }
+
